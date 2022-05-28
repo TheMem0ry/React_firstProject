f896b12a641b3ed6ff5cf934214bf1a88747367f
Maksym Koretskyi <i.koretskyi.maksym@gmail.com>
2022-05-28 20:56:21 +0300

    Preparing to add the usePagination hook & added pagination

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import PostItem from "./Components/PostItem";
 import Counter from "./Components/Counter";
 import ClassCounter from "./Components/ClassCounter";
@@ -8,22 +8,36 @@ import MyButton from "./Components/UI/button/MyButton";
 import MyInput from "./Components/UI/input/MyInput";
 import PostForm from "./Components/PostForm";
 import MySelect from "./Components/UI/select/MySelect";
-import PostFilter from "./Components/UI/PostFilter";
+import PostFilter from "./Components/PostFilter";
 import MyModal from "./Components/UI/MyModal/MyModal";
 import {usePosts} from "./hooks/usePosts.js"
+import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./Components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages.js"
 
 function App() {
-
-  const [posts, setPosts] = useState([
-    {id:1, title:'JavaScript', body:'Description'},
-    {id:2, title:'React', body:'Description'},
-    {id:5, title:'HTML/CSS', body:'Description'}
-  ])
-  
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort:'', query:''})
   const [modal, setModal] = useState(false)
-
   const sortedAndSearchedPosts = usePosts(posts,filter.sort, filter.query)
+  const [totalPages, setTotalPages] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
+
+  let pagesArray = getPagesArray(totalPages)
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching( async () => {
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit))
+  })
+
+  useEffect(() => {
+    fetchPosts()
+  }, [page])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -34,11 +48,12 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
- 
+  const changePage = (page) => {
+    setPage(page)
+  }
 
   return (
     <div className="App">
-      
       <MyButton style={{marginTop:'30px'}}
       onClick={() => setModal(true)}
       >Create Post</MyButton>
@@ -53,10 +68,29 @@ function App() {
       <PostFilter filter = {filter} 
       setFilter = {setFilter}
       />
-       <PostList
-          posts={sortedAndSearchedPosts}
-          title='Post List Title'
-          remove={removePost}/>
+      {postError &&
+      <h1>There was a fetching error {postError}</h1>
+      }
+
+      {isPostsLoading
+      ? <div style={{display:'flex', justifyContent:'center', marginTop:'20px'}}><Loader></Loader></div>
+      : <PostList
+      posts={sortedAndSearchedPosts}
+      title='Post List Title'
+      remove={removePost}/>
+      }
+     
+     <div className="pages__wrapper">
+        {pagesArray.map((p) => 
+          <span 
+          className={page === p ? 'page page__current' : 'page'} 
+          key={p}
+          onClick={() => changePage(p)}
+          >
+            {p}
+            </span>
+          )}
+     </div>
     </div>
   );
 }
